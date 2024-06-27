43cba16f3423561fd9a7f960f60f2ad383e645af
Alex.F <gfreezy@gmail.com>
2024-06-27 18:11:34 +0800

    chore: Update MailUrlGen to use web_domain instead of mail_domain

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,13 @@ fn main() -> Result<()> {
     let feishu_app_secret =
         std::env::var("FEISHU_APP_SECRET").expect("`FEISHU_APP_SECRET` must be set");
     let mail_domain = std::env::var("MAIL_DOMAIN").expect("`MAIL_DOMAIN` must be set");
+    let web_domain = std::env::var("WEB_DOMAIN").expect("`WEB_DOMAIN` must be set");
     let store_path = std::env::var("STORE_PATH").unwrap_or_else(|_| "store.sqlite".to_string());
     let client = Client::new(feishu_app_id, feishu_app_secret.clone());
     let client_clone = client.clone();
     let store = Store::new(Some(store_path), mail_domain.clone())?;
     let store_clone = store.clone();
-    let mail_url_gen = MailUrlGen::new(mail_domain, feishu_app_secret);
+    let mail_url_gen = MailUrlGen::new(web_domain, feishu_app_secret);
     let mail_url_gen_clone = mail_url_gen.clone();
     thread::spawn(move || smtp_server::serve(client_clone, store_clone, mail_url_gen_clone));
     bot_server::serve(client, store, mail_url_gen)?;
