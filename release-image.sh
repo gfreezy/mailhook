77f56361b4f58299a0eb417efb63dbd3f38c06a6
gfreezy <gfreezy@gmail.com>
2024-06-27 23:44:47 +0800

    chore: Update mailhook version to 2.0.0 in Cargo.toml and Cargo.lock

--- release-image.sh
+++ release-image.sh
@@ -2,8 +2,6 @@
 # Get version from Cargo.toml
 VERSION=$(grep version Cargo.toml | sed -n 's/.*version = "\([^"]*\)".*/\1/p;q')
 echo $VERSION
-git tag $VERSION
-git push $VERSION
 docker build -t gfreezy/mailhook:$VERSION -t gfreezy/mailhook:latest .
 docker push gfreezy/mailhook:$VERSION
 docker push gfreezy/mailhook:latest
